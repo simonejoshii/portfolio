6aefbaf3e83613487968ad8a1fb707d6aefdb2fc
simonejoshii <161744470+simonejoshii@users.noreply.github.com>
2024-12-25 15:20:18 +0530

    Add files via upload

--- simonejoshi.js
+++ simonejoshi.js
@@ -1,11 +1,3 @@
-/*!
- * Webflow: Front-end site library
- * @license MIT
- * Inline scripts may access the api using an async handler:
- *   var Webflow = Webflow || [];
- *   Webflow.push(readyFunction);
- */
-
 (()=>{
     var aC = Object.create;
     var Mi = Object.defineProperty;
